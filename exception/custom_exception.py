b0842ad534814998f0027f57fd4d7e71a8ff75af
Parth Billore <152008377+billoreparth@users.noreply.github.com>
2025-06-30 23:23:37 +0530

    Update custom_exception.py

--- exception/custom_exception.py
+++ exception/custom_exception.py
@@ -1,13 +1,15 @@
 import sys
 
-class custom_exception(Exception):
-    def __init__(self,error_message,error_details:sys):
-        self.error_message=error_message
-        _,_,exc_tb=error_details.exc_info()
-        self.line_no=exc_tb.tb_lineno
+class custom_exception(Exception):  
+    def __init__(self, error_message, error_details: sys):
+        super().__init__(error_message)  
+        _, _, exc_tb = error_details.exc_info()
+        self.line_no = exc_tb.tb_lineno
         self.file_name = exc_tb.tb_frame.f_code.co_filename
+        self.error_message = error_message
 
     def __str__(self):
-        return f"the error has occured in file :{0} in line number : {1} and error message is {2}".format(self.file_name,self.line_no,str(self.error_message))
+        return f"The error occurred in file: {self.file_name} on line {self.line_no}. Error message: {str(self.error_message)}"
+
         
 
